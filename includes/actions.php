fe3f5cfe70ec69024297540f52fb8e5cdc06fabe
Daniel Powney <danielpowney@gmail.com>
2017-07-10 20:09:35 +1000

    Removed general post and get actions for MYC

--- includes/actions.php
+++ includes/actions.php
@@ -12,36 +12,3 @@
 // Exit if accessed directly
 if (! defined ( 'ABSPATH' ))
 	exit ();
-
-/**
- * Hooks MYC actions, when present in the $_GET superglobal.
- * Every edd_action
- * present in $_GET is called using WordPress's do_action function. These
- * functions are called on init.
- *
- * @since 0.1
- * @return void
- */
-function myc_get_actions() {
-	if (isset ( $_GET ['myc_action'] )) {
-		do_action ( 'myc_' . $_GET ['myc_action'], $_GET );
-	}
-}
-add_action ( 'init', 'myc_get_actions' );
-
-/**
- * Hooks MYC actions, when present in the $_POST superglobal.
- * Every edd_action
- * present in $_POST is called using WordPress's do_action function. These
- * functions are called on init.
- *
- * @since 0.1
- * @return void
- *
- */
-function myc_post_actions() {
-	if (isset ( $_POST ['myc_action'] )) {
-		do_action ( 'myc_' . $_POST ['myc_action'], $_POST );
-	}
-}
-add_action ( 'init', 'myc_post_actions' );
